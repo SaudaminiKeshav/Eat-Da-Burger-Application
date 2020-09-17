5c2105b675ee82a87d48c5dbfb6a284d8ac2ae97
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-09-17 07:13:20 -0700

    Added config files and created database

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,11 @@
-### Schema
 
-CREATE DATABASE cat_db;
-USE cat_db;
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
-CREATE TABLE cats
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+CREATE TABLE burgers (
+id INTEGER NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(100) NOT NULL,
+devoured BOOLEAN,
+PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,10 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (burger_name, devoured) 
+VALUES 
+("The Impossible Burger", false),
+("Mediterranean Veggie Burger", false),
+("Vegan Buffalo Wings Burger", false),
+("Black Bean Burger", false),
+("Portobello Mushroom Burger", true),
+("Vegan Sloppy Joe", false);
+
+SELECT * FROM burgers;
