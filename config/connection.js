3cb079f92688881a87dd9c8e6acd07e10015719a
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-09-20 17:48:44 -0700

    Update and bug fixes

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,6 @@ var mysql = require("mysql");
 
 let connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -13,7 +10,7 @@ if (process.env.JAWSDB_URL) {
     password: "password",
     database: "burgers_db"
   })
-};
+
 // Make connection.
 connection.connect(function (err) {
   if (err) {
