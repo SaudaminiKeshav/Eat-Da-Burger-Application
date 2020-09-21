2a37adba30f427ddba16a3b0aa946f4f2de2a431
Saudamini Keshav <saudamini.keshav@gmail.com>
2020-09-20 18:33:45 -0700

    Deleted and updated package.json

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,9 @@ var mysql = require("mysql");
 
 let connection;
 
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
   connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -10,7 +13,7 @@ let connection;
     password: "password",
     database: "burgers_db"
   })
-
+};
 // Make connection.
 connection.connect(function (err) {
   if (err) {
