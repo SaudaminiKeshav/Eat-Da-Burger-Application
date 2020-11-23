1ef0cbc89959eed2307d16358e59696ebcdedf31
Saudamini Keshav <Saudamini.Keshav@gmail.com>
2020-11-22 16:34:39 -0800

    Updated ORM queries

--- models/burger-model.js
+++ models/burger-model.js
@@ -1,34 +1,26 @@
 const orm = require('../config/orm.js');
 
+// Code that will call the ORM functions using burger specific input for the ORM.
 const burger = {
     all: function (cb) {
-        orm.all("burgers", function (res) {
-            console.log("All results from burgers table: ", res);
-            cb(res);
-        });
+        // SELECT * FROM burgers
+        // Returns RowDataPacket array results from burgers table to router
+        orm.all("burgers", res => cb(res));
     },
     // The variables cols and vals are arrays.
     create: function (cols, vals, cb) {
         // INSERT INTO burgers VALUES ?
-        orm.create("burgers", cols, vals, function (res) {
-            cb(res);
-        });
+        orm.create("burgers", cols, vals, res => cb(res));
     },
-
     // UPDATE burgers SET columns WHERE condition
     update: function (objColVals, condition, cb) {
-        orm.update("burgers", objColVals, condition, function (res) {
-            cb(res);
-        });
+        orm.update("burgers", objColVals, condition, res => cb(res));
     },
-
-      // DELETE FROM burgers WHERE condition
-      delete: function(condition, cb) {
-        orm.delete("burgers", condition, function(res) {
-          cb(res);
-        });
-      }
+    // DELETE FROM burgers WHERE condition
+    delete: function (condition, cb) {
+        orm.delete("burgers", condition, res => cb(res));
+    }
 };
 
 // Export the database functions for the controller (burgers_controller.js).
-module.exports = burger; 
+module.exports = burger;
